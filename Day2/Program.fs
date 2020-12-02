586befbe9b4c557cc7ba87cb8f2bde9e7edc1041
filipni <filip.nilsson+github@protonmail.com>
2020-12-02 14:14:40 +0100

    Add day 2 solutions

---
+++ Day2/Program.fs
@@ -0,0 +1,34 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let input =
+    let extractValues str = 
+        Regex.Match(str, @"(\d+)-(\d+) (\w): (.+)").Groups
+        |> fun groups -> (int groups.[1].Value, int groups.[2].Value, char groups.[3].Value, groups.[4].Value)
+
+    File.ReadAllLines("../../../input.txt")
+    |> Array.map extractValues
+
+let part1 =
+    let count x = Seq.filter ((=) x) >> Seq.length
+
+    input
+    |> Array.map (fun (min, max, c, password) -> (min, max, (count c password)))
+    |> Array.filter (fun (min, max, count) -> count >= min && count <= max)
+    |> Array.length
+
+let part2 =
+    let isValid (index1: int, index2: int, c: char, password: string) =
+        let indices = [1 .. password.Length]
+        let matchingIndices = List.filter (fun i -> password.[i - 1] = c) indices
+        List.exists ((=) index1) matchingIndices <> List.exists ((=) index2) matchingIndices
+
+    input
+    |> Array.filter isValid
+    |> Array.length
+
+[<EntryPoint>]
+let main _ =
+    printfn "Answer part 1: %d" part1
+    printfn "Answer part 2: %d" part2
+    0
