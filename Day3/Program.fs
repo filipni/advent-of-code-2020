73030770998b46b4c360f43420f670850fcbe251
filipni <filip.nilsson+github@protonmail.com>
2020-12-03 12:38:11 +0100

    Add solutino for day 3 part 2

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -3,18 +3,37 @@
 let input =
     File.ReadAllLines(@"../../../input.txt") |> Array.toList
 
-let rec traversePath pos map treeSum =
+let rec updateMap yDiff map = 
+    match yDiff, map with
+    | 0, _ -> map
+    | _, [] -> []
+    | _, (_::xs) -> updateMap (yDiff - 1) xs 
+
+let rec traversePath pos map xDiff yDiff treeSum =
     match map with
     | [] -> treeSum
-    | (x::xs) ->
-        let newPosition = (pos + 3) % String.length x
+    | (x::_) ->
+        let newPosition = (pos + xDiff) % String.length x
         let updatedTreeSum = if x.[pos] = '#' then treeSum + 1 else treeSum
-        traversePath newPosition xs updatedTreeSum
+        let updatedMap = updateMap yDiff map
+        traversePath newPosition updatedMap xDiff yDiff updatedTreeSum
 
 let part1 =
-    traversePath 0 input 0
+    traversePath 0 input 3 1 0
+
+let part2 =
+    [
+        traversePath 0 input 1 1 0
+        traversePath 0 input 3 1 0
+        traversePath 0 input 5 1 0
+        traversePath 0 input 7 1 0
+        traversePath 0 input 1 2 0
+    ]
+    |> List.map int64
+    |> List.reduce (fun x y -> x * y) 
 
 [<EntryPoint>]
 let main _ =
     printfn "Answer part 1: %d" part1 
+    printfn "Answer part 2: %d" part2 
     0
