78dac843c887f48a209712098c87eda8a40df21f
filipni <filip.nilsson+github@protonmail.com>
2020-12-03 17:36:03 +0100

    Refactor day 3

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -3,26 +3,25 @@
 let input =
     File.ReadAllLines(@"../../../input.txt") |> Array.toList
 
-let rec traversePath pos map xDiff yDiff treeSum =
+let rec traversePath xPos map xDiff yDiff (treesHit: int64) =
     match map with
-    | [] -> treeSum
+    | [] -> treesHit
     | (x::_) ->
-        let newPosition = (pos + xDiff) % String.length x
-        let updatedTreeSum = if x.[pos] = '#' then treeSum + 1 else treeSum
-        traversePath newPosition map.[yDiff..]  xDiff yDiff updatedTreeSum
+        let newXPos = (xPos + xDiff) % String.length x
+        let updatedTreeSum = if x.[xPos] = '#' then treesHit + 1L else treesHit
+        traversePath newXPos map.[yDiff..] xDiff yDiff updatedTreeSum
 
 let part1 =
-    traversePath 0 input 3 1 0
+    traversePath 0 input 3 1 0L
 
 let part2 =
     [
-        traversePath 0 input 1 1 0
-        traversePath 0 input 3 1 0
-        traversePath 0 input 5 1 0
-        traversePath 0 input 7 1 0
-        traversePath 0 input 1 2 0
+        traversePath 0 input 1 1 0L
+        traversePath 0 input 3 1 0L
+        traversePath 0 input 5 1 0L
+        traversePath 0 input 7 1 0L
+        traversePath 0 input 1 2 0L
     ]
-    |> List.map int64
     |> List.reduce (fun x y -> x * y) 
 
 [<EntryPoint>]
