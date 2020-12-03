c1a0e21f2346d5f386c088d8fd21727a5e9cb815
filipni <filip.nilsson+github@protonmail.com>
2020-12-03 11:04:15 +0100

    Add soultion for day 3 part 1

---
+++ Day3/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+
+let input =
+    File.ReadAllLines(@"../../../input.txt") |> Array.toList
+
+let rec traversePath pos map treesHit =
+    match map with
+    | [] -> treesHit
+    | (x::xs) -> traversePath ((pos + 3) % String.length x) xs (if x.[pos] = '#' then treesHit + 1 else treesHit)
+
+let part1 =
+    traversePath 0 input 0
+
+[<EntryPoint>]
+let main _ =
+    printfn "Answer part 1: %d" part1 
+    0
