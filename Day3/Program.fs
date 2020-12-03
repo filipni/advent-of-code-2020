066af2ce166f450cb9b7abbc58b3990dd804324f
filipni <filip.nilsson+github@protonmail.com>
2020-12-03 19:47:22 +0100

    Add alternative solution to day 3

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -3,26 +3,31 @@
 let input =
     File.ReadAllLines(@"../../../input.txt") |> Array.toList
 
-let rec traversePath xPos map xDiff yDiff (sum: int64) =
+let rec traversePathRecursive xPos map xDiff yDiff (sum: int64) =
     match map with
     | [] -> sum
-    | (x::_) ->
+    | x::_ ->
         let newXPos = (xPos + xDiff) % String.length x
         let newSum = if x.[xPos] = '#' then sum + 1L else sum
-        traversePath newXPos map.[yDiff..] xDiff yDiff newSum
+        traversePathRecursive newXPos map.[yDiff..] xDiff yDiff newSum
+
+let traversePath (map: string list) xDiff yDiff =
+    let numPositions = if yDiff = 1 then map.Length else map.Length / yDiff + 1
+    [for i in 0..numPositions-1 -> (i * xDiff % map.[0].Length, i * yDiff)]
+    |> List.fold (fun sum (x, y) -> if map.[y].[x] = '#' then sum + 1L else sum) 0L
 
 let part1 =
-    traversePath 0 input 3 1 0L
+    traversePath input 3 1
 
 let part2 =
-    [
-        traversePath 0 input 1 1 0L
-        traversePath 0 input 3 1 0L
-        traversePath 0 input 5 1 0L
-        traversePath 0 input 7 1 0L
-        traversePath 0 input 1 2 0L
+    let result = [
+        traversePath input 1 1
+        traversePath input 3 1
+        traversePath input 5 1
+        traversePath input 7 1
+        traversePath input 1 2
     ]
-    |> List.reduce (fun x y -> x * y) 
+    result |> List.reduce (fun x y -> x * y) 
 
 [<EntryPoint>]
 let main _ =
