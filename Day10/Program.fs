e0a38c45681c6b66bda6203b9de4a434d764d946
filipni <filip.nilsson+github@protonmail.com>
2020-12-10 12:02:31 +0100

    Add solution for day 10 part 1

---
+++ Day10/Program.fs
@@ -0,0 +1,32 @@
+﻿open System.IO
+
+let input = File.ReadAllLines(@"../../../input.txt") |> Array.map int |> Array.sort
+
+let updateCount (count: Map<int, int>) diff =
+    if count.ContainsKey(diff) then
+        count.Add(diff, count.[diff] + 1)
+    else
+        count.Add(diff, 1)
+
+let rec findChain jolt (count: Map<int, int>) adapters =
+    let compatibleAdapters =
+        adapters
+        |> Array.mapi (fun i x -> (i, x, x - jolt))
+        |> Array.filter (fun (_, _, diff) -> diff <= 3)
+
+    if Array.isEmpty compatibleAdapters then
+       let finalCount = updateCount count 3
+       finalCount.[1] * finalCount.[3]
+    else
+        let (index, adapter, diff) = Array.head compatibleAdapters
+        findChain adapter (updateCount count diff) (adapters.[index+1..])
+
+let part1 = findChain 0 Map.empty input
+
+let part2 = 0
+
+[<EntryPoint>]
+let main _ =
+    printfn $"Answer part 1: {part1}"
+    printfn $"Answer part 2: {part2}"
+    0
