df65009ea43a17c97cc78a680adb10f772fb83a7
filipni <filip.nilsson+github@protonmail.com>
2020-12-10 23:05:17 +0100

    Add solution for day 10 part 2

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -2,28 +2,43 @@
 
 let input = File.ReadAllLines(@"../../../input.txt") |> Array.map int |> Array.sort
 
+let getCompatibleAdapters jolt adapters =
+    adapters
+    |> Array.mapi (fun i x -> (i, x, x - jolt))
+    |> Array.filter (fun (_, _, diff) -> diff <= 3)
+
 let updateCount (count: Map<int, int>) diff =
-    if count.ContainsKey(diff) then
-        count.Add(diff, count.[diff] + 1)
-    else
-        count.Add(diff, 1)
+    match count.TryFind(diff) with
+    | Some(value) -> count.Add(diff, count.[diff] + 1)
+    | None -> count.Add(diff, 1)
 
 let rec findChain jolt (count: Map<int, int>) adapters =
-    let compatibleAdapters =
-        adapters
-        |> Array.mapi (fun i x -> (i, x, x - jolt))
-        |> Array.filter (fun (_, _, diff) -> diff <= 3)
-
-    if Array.isEmpty compatibleAdapters then
-       let finalCount = updateCount count 3
-       finalCount.[1] * finalCount.[3]
-    else
+    match getCompatibleAdapters jolt adapters with
+    | [||] -> 
+        let finalCount = updateCount count 3
+        finalCount.[1] * finalCount.[3]
+    | compatibleAdapters -> 
         let (index, adapter, diff) = Array.head compatibleAdapters
-        findChain adapter (updateCount count diff) (adapters.[index+1..])
+        findChain adapter (updateCount count diff) adapters.[index+1..]
+
+let rec sumChains jolt adapters =
+    match getCompatibleAdapters jolt adapters with
+    | [||] -> 1L
+    | compatibleAdapters ->
+        let length = Array.length compatibleAdapters
+        if length = 2 then
+            let (index, adapter, _) = compatibleAdapters.[0]
+            2L * sumChains adapter adapters.[index+1..]
+        elif length =  3 then
+            let (index2, adapter2, _) = compatibleAdapters.[1]
+            let (index3, adapter3, _) = compatibleAdapters.[2]
+            3L * sumChains adapter2 adapters.[index2+1 ..] + sumChains adapter3 adapters.[index3+1 ..]
+        else
+            let (index, adapter, _) = compatibleAdapters.[0]
+            sumChains adapter adapters.[index+1 ..]
 
 let part1 = findChain 0 Map.empty input
-
-let part2 = 0
+let part2 = sumChains 0 input
 
 [<EntryPoint>]
 let main _ =
